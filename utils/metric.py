960b94ab8380a3f4a0847f83a96fcbed1c0893bd
Petecheco <sunshengkeofficial@gmail.com>
2024-07-12 22:03:51 +0800

    update

--- utils/metric.py
+++ utils/metric.py
@@ -4,6 +4,19 @@ import torch.nn.functional as F
 from sklearn.metrics import precision_score, recall_score, f1_score
 
 
+def GetOutputs(model, dataloader, device="cpu"):
+    outputs = []
+    y_true = []
+    with torch.no_grad():
+        model.to(device)
+        model.eval()
+        for data, labels in dataloader:
+            data = data.to(device)
+            outputs.extend(model(data).detach().cpu())
+            y_true.extend(labels.detach().cpu())
+    return outputs, y_true
+
+
 def OutputToPredictions(outputs):
     probabilities = F.softmax(outputs, dim=1)
     predictions = torch.argmax(probabilities, dim=1)
@@ -34,3 +47,20 @@ def CalculateF1(y_true, outputs):
     predictions = OutputToPredictions(outputs)
     score = f1_score(y_true, predictions, average="macro")
     return score
+
+
+def CalculateCrossEntropy(y_true, outputs):
+    return F.cross_entropy(outputs, y_true).numpy()
+
+
+if __name__ == '__main__':
+    outputs = torch.tensor([[0.1, 0.9],
+                            [0.9, 0.1],
+                            [0.1, 0.9],
+                            [0.9, 0.1],
+                            [0.1, 0.9],
+                            [0.9, 0.1],
+                            [0.1, 0.9]])
+    y_true = torch.tensor((1, 1, 1, 0, 1, 0, 1))
+    acc = CalculateCrossEntropy(y_true, outputs)
+    print(acc)
