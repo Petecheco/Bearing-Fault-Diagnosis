2d7e6fa9391b5182cc5273c30ce959f2351d25c9
Petecheco <sunshengkeofficial@gmail.com>
2024-10-23 14:49:08 +0800

    add CalculatePerclassAcc

--- utils/metric.py
+++ utils/metric.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from sklearn.metrics import precision_score, recall_score, f1_score
-
+from collections import defaultdict
 
 def GetOutputs(model, dataloader, device="cpu"):
     outputs = []
@@ -56,6 +56,16 @@ def CalculateCrossEntropy(y_true, outputs):
 def CalculateParameters(model):
     return sum(p.numel() for p in model.parameters())
 
+def CalculatePerClassAccuracy(y_true, outputs):
+    predictions = OutputToPredictions(outputs)
+    correct_counts = defaultdict(int)
+    total_counts = defaultdict(int)
+    for pred, label in zip(predictions, y_true):
+        if pred == label:
+            correct_counts[int(label)] += 1
+        total_counts[int(label)] += 1
+    accuracies = {label: correct / total_counts[label] if total_counts[label] != 0 else 0 for (label, correct) in correct_counts.items()}
+    return accuracies
 
 if __name__ == '__main__':
     outputs = torch.tensor([[0.1, 0.9],
@@ -66,5 +76,5 @@ if __name__ == '__main__':
                             [0.9, 0.1],
                             [0.1, 0.9]])
     y_true = torch.tensor((1, 1, 1, 0, 1, 0, 1))
-    acc = CalculateCrossEntropy(y_true, outputs)
+    acc = CalculatePerClassAccuracy(y_true, outputs)
     print(acc)
